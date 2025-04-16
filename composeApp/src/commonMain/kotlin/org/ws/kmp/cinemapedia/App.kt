8f3a6284f8ec589eb5759cc8bfcfed433a481d8e
AbielRC97 <109878636+AbielRC97@users.noreply.github.com>
2025-04-16 14:30:03 -0600

    Implementacion de Coil para manejo de imagenes

--- composeApp/src/commonMain/kotlin/org/ws/kmp/cinemapedia/App.kt
+++ composeApp/src/commonMain/kotlin/org/ws/kmp/cinemapedia/App.kt
@@ -22,7 +22,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import coil3.compose.AsyncImage
 import androidx.compose.ui.unit.dp
+import coil3.ImageLoader
+import coil3.annotation.ExperimentalCoilApi
+import coil3.compose.setSingletonImageLoaderFactory
+import coil3.request.crossfade
+import coil3.util.DebugLogger
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
@@ -46,11 +53,19 @@ fun App() {
     }
 }
 
+@OptIn(ExperimentalCoilApi::class)
 @Composable
 fun MovieItem(movie: Movie) {
     Column {
-        Box(
-            modifier =  Modifier.fillMaxWidth().aspectRatio(2/3f).clip(MaterialTheme.shapes.small).background(MaterialTheme.colorScheme.primaryContainer)
+        setSingletonImageLoaderFactory { context -> ImageLoader.Builder(context)
+            .crossfade(true)
+            .logger(DebugLogger())
+            .build()  }
+        AsyncImage(
+            model = movie.poster,
+            contentDescription =  movie.title,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxWidth().aspectRatio(2/3f).clip(MaterialTheme.shapes.small)
         )
         Text(
             text = movie.title,
