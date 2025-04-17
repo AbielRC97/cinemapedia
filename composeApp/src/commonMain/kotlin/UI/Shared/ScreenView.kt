2ea00636b67fc21a1d114f8608bab5800be73322
AbielRC97 <109878636+AbielRC97@users.noreply.github.com>
2025-04-16 21:51:18 -0600

    TopAppBar DarkMode

--- composeApp/src/commonMain/kotlin/UI/Shared/ScreenView.kt
+++ composeApp/src/commonMain/kotlin/UI/Shared/ScreenView.kt
@@ -1,15 +1,20 @@
 package UI.Shared
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
 fun ScreenView (modifier: Modifier = Modifier, content: @Composable () -> Unit){
+    var isDarkMode = isSystemInDarkTheme()
+    val colorScheme = if(isDarkMode)  darkColorScheme() else lightColorScheme()
     MaterialTheme(
-
+    colorScheme
     ) {
         Surface(
             modifier = Modifier.fillMaxWidth(),
